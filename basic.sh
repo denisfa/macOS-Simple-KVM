52441dcdfa5e1c47cdc8cbfd70edb1e26d30928d
Edênis Freindorfer Azevedo <edenisfa@gmail.com>
2021-07-09 11:03:08 -0300

    Update script for daily usage.

--- basic.sh
+++ basic.sh
@@ -8,7 +8,7 @@ OVMF=$VMDIR/firmware
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 2G \
+    -m 4G \
     -machine q35,accel=kvm \
     -smp 4,cores=2 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
@@ -19,10 +19,12 @@ qemu-system-x86_64 \
     -vga qxl \
     -device ich9-intel-hda -device hda-output \
     -usb -device usb-kbd -device usb-mouse \
-    -netdev user,id=net0 \
-    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+    -netdev user,id=net0,hostfwd=tcp::2222-:22,smb=$HOME/,smbserver=10.0.2.4 \
+    -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
     -device ich9-ahci,id=sata \
-    -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
-    -device ide-hd,bus=sata.2,drive=ESP \
     -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
+    -device ide-hd,bus=sata.2,drive=ESP \
+    -drive id=SystemDisk,if=none,file=/mnt/raid0/denisfa/MyDisk.qcow2 \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
